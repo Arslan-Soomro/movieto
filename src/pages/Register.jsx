c66c4bc038e486fb1cb4c7804c64079a49d5785c
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-09-23 07:42:19 -0700

    Setup SideImage Properly At Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,10 +9,10 @@ let Register = () => {
     return (
         <section className="font-poppins container h-full flex items-center justify-between">
             <Navbar />
-            <div className="flex-grow flex justify-center">
+            <div className="flex-grow sm:flex-grow-0 sm:w-9/12 flex justify-center">
                 {signup ? <Signup /> : <Login />}
             </div>
-            <div className="hidden sm:block bg-purple-600 h-full w-1/3">
+            <div className="hidden sm:block absolute right-0 bg-purple-600 h-full w-3/12">
                 <img src="images/side-asset.png" className="h-full w-full object-cover"></img>
             </div>
         </section>
