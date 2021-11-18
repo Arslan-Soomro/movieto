618e63d96c6e729ede9613bc9c2c4ffe3af02bf3
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-11-18 06:23:06 -0800

    Add Routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,18 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import MovieModal from "./components/MovieModal";
 import movieScrap from "./scripts/movieScraper";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
 
   return (
   <div className="App h-full">
     <Navbar />
-    {/*
-    
-    <RTemplate><Signup /></RTemplate>
-    */}
-
-    <Home />
-    
-    {/*
-    <MovieModal />
-    */}
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/signup" element={<RTemplate><Signup /></RTemplate>} />
+      <Route path="/login" exact element={<RTemplate><Login /></RTemplate>} />
+    </Routes>
   </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 let Navbar = () => {
   return (
     <div className="absolute z-10 top-2 xs:top-4 w-full">
       <div className="container px-3 sm:px-10 font-poppins flex items-center justify-between md:justify-start">
-        <h3 className="font-bold xs:text-xl text-purple-600">Movieto</h3>
+        <Link to="/">
+          <h3 className="font-bold xs:text-xl text-purple-600">Movieto</h3>
+        </Link>
         <div className="text-purple-600 text-sm xs:text-base">
-          <button className="bg-purple-50 hover:bg-purple-100 active:bg-purple-200 mx-4 px-3 py-1 rounded-full cursor-pointer">
-            Login
-          </button>
+          <Link to="/login">
+            <button className="bg-purple-50 hover:bg-purple-100 active:bg-purple-200 mx-4 px-3 py-1 rounded-full cursor-pointer">
+              Login
+            </button>
+          </Link>
+          <Link to="/signup">
           <button className="bg-purple-50 hover:bg-purple-100 active:bg-purple-200 px-3 py-1 rounded-full cursor-pointer">Signup</button>
+          </Link>
         </div>
       </div>
     </div>
