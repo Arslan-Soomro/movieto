57431eedca1a8452c0f8de328318b252f2ab04cf
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-12-18 08:27:35 -0800

    Changes to global.js

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,4 +1,3 @@
-//export const API_URL = 'http://localhost:5000';
-export const API_URL = 'https://movieto-api.vercel.app/';
+export const API_URL = 'https://localhost:5000/';
 export const TOKEN_NAME = 'MovietoToken';
 export const INVALID_TOKEN_SIGN = "NAN_TOKEN";
